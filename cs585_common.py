dd2ad7b127fb151949b6ffddb4f7636d86167bc7
timevdo <timevdo@bu.edu>
2024-04-18 11:32:13 -0400

    Wheel angle done

--- cs585_common.py
+++ cs585_common.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def crop_video(input_video_path, output_video_path, start_time, end_time):
     # Open the video file
@@ -36,4 +37,42 @@ def crop_video(input_video_path, output_video_path, start_time, end_time):
     # Release video capture and writer
     video_capture.release()
     output_video.release()
+
+def segment_hands(frame):
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    blurred_frame = cv2.GaussianBlur(hsv_frame, (11, 11), 0)
+
+    lower = np.array([0, 10, 60], dtype = "uint8") 
+    upper = np.array([20, 150, 255], dtype = "uint8")
+
+    mask = cv2.inRange(blurred_frame, lower, upper)
+
+    # Find contours
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Find the contour with the largest area
+    sorted_contours = sorted(contours, key=cv2.contourArea)
+
+    hand1 = sorted_contours[-1]
+    hand2 = sorted_contours[-2]
+
+    def center_of_contours(c):
+        M = cv2.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+        return (cX, cY)
+    
+    c1 = center_of_contours(hand1)
+    c2 = center_of_contours(hand2)
+
+    if (c1[0] > c2[0]):
+        return (c2, c1)
+    return (c1, c2)
+
+def angle_of_wheel_line(c1, c2):
+    x1, y1 = c1
+    x2, y2 = c2
+
+    return np.rad2deg(np.arctan2((y2 - y1), (x2 - x1)))
 
