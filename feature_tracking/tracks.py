a73af611a61780a73b8ac394c123433fed9c025d
aseef <contact@aseef.dev>
2024-04-21 11:59:38 -0400

    combined stuff into a function for easy integration into notebook

--- feature_tracking/tracks.py
+++ feature_tracking/tracks.py
@@ -53,47 +53,23 @@ def load_template_pyramid(template_path, down_levels, up_levels, scale_factor=0.
     return template_pyramid
 
 
-# Initialize feature detector
-orb = cv2.ORB.create()
+def init_feature_tracking():
+    # Initialize feature detector
+    orb = cv2.ORB.create()
+    # Initialize a matcher
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    return orb, bf
 
-# Initialize a matcher
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-# Open video file
-cap = cv2.VideoCapture("../data/audi_road_cropped.mp4")
-
-# Read the first frame
-ret, prev_frame = cap.read()
-prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-
-# Find keypoints and descriptors in the first frame
-prev_kp, prev_des = orb.detectAndCompute(prev_gray, None)
-
-template_match_history = []
-
-# Initialize template for speedometer
-speedometer_templates = load_template_pyramid("../data/speed_template.png", 2, 1)
-
-orginal_center = None
-offset = None
-predicted_center = None
-
-cap.set(cv2.CAP_PROP_POS_FRAMES, 3050)
-
-pt_color_dict = {}
-
-while True:
-    # Read the next frame
-    ret, frame = cap.read()
-    if not ret:
-        break
-
+def find_speedometer(frame, orb, bf, prev_kp, prev_des, speedometer_templates,
+                     template_match_history, pt_color_dict, predicted_center, debug=False):
     # Find the speedometer
-    template_center, top_left, bottom_right = find_speedometer_template_matching(frame, speedometer_templates, threshold=0.0)
+    template_center, top_left, bottom_right = find_speedometer_template_matching(frame, speedometer_templates,
+                                                                                 threshold=0.0)
     template_match_history += [template_center]
 
     # If a match is found, draw it
-    if template_center is not None:
+    if template_center is not None and debug:
         cv2.rectangle(frame, top_left, bottom_right, (0, 255, 255), 2)
         cv2.circle(frame, template_center, 5, (255, 0, 0), -1)
         cv2.circle(frame, top_left, 5, (255, 0, 255), -1)
@@ -106,21 +82,25 @@ while True:
     if template_center is not None and len(template_match_history) > 10:
         if all([x is not None and math.dist(template_center, x) < 15 for x in template_match_history[-5:]]):
             predicted_center = template_center
-    template_match_history = template_match_history[-10:]
+
+    temp_history = template_match_history[-10:]
+    template_match_history.clear()
+    template_match_history.extend(temp_history)
 
     # Convert frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Find keypoints and descriptors in the current frame
-    kp, des = orb.detectAndCompute(gray, None)
+    kp, des = orb.detectAndCompute(gray_frame, None)
 
-    if des is not None:
+    if des is not None and prev_des is not None and prev_kp is not None and kp is not None:
 
         # Match descriptors between the current and previous frames
         matches = bf.match(prev_des, des)
 
         # Initialize variables to store matched keypoints and their vectors for this frame
         vectors_frame = []
+        matched_kps = []
 
         # Process each match
         for match in matches:
@@ -129,7 +109,28 @@ while True:
             cur_kp_match = kp[match.trainIdx]
             prev_pt = prev_kp_match.pt
             cur_pt = cur_kp_match.pt
+            # If we don't have a predicted center, skip
+            if predicted_center is None:
+                continue
+            # Check if the matched keypoint is outside the template bounding box
+            # skip if it is
+            if cur_pt[1] < predicted_center[1]:
+                continue
+            matched_kps.append((prev_kp_match, cur_kp_match))
 
+        # Filter out weak matches by removing matches with a response less than the 0.5 std below the mean response
+        responses = [kp.response for kp in kp]
+        mean_response = np.mean(responses)
+        std_response = np.std(responses)
+        matched_kps = [(prev_kp, cur_kp) for prev_kp, cur_kp in matched_kps
+                       if cur_kp.response > mean_response - (std_response * 0.5)]
+
+        for matched_kp in matched_kps:
+            prev_kp_match, cur_kp_match = matched_kp
+            prev_pt = prev_kp_match.pt
+            cur_pt = cur_kp_match.pt
+
+            # Match keypoints with colors such that the same keypoint will have the same color
             if prev_pt in pt_color_dict:
                 color = pt_color_dict[prev_pt]
                 del pt_color_dict[prev_pt]
@@ -137,22 +138,17 @@ while True:
             else:
                 pt_color_dict[cur_pt] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-            # If we don't have a predicted center, skip
-            if predicted_center is None:
-                continue
-
-            # Check if the matched keypoint is outside the template bounding box
-            # skip if it is
-            if cur_pt[1] < predicted_center[1]:
-                continue
-
-            cv2.circle(frame, (int(cur_pt[0]), int(cur_pt[1])), 5, pt_color_dict[cur_pt], -1)
+            # Visualize the matched keypoints
+            if debug:
+                cv2.circle(frame, (int(cur_pt[0]), int(cur_pt[1])), 5, pt_color_dict[cur_pt], -1)
 
             # Calculate vector from previous point to current point
             vector = np.array([cur_pt[0] - prev_pt[0], cur_pt[1] - prev_pt[1]])
 
             # Append matched keypoint and vector to lists
             vectors_frame.append(vector)
+
+        mean_vector = None
 
         # Compute the mean vector from the top 25% of the vectors that moved the least
         if vectors_frame:
@@ -164,25 +160,58 @@ while True:
             mean_vector = np.mean(vectors_frame, axis=0)
 
         # Draw vector
-        if vectors_frame and mean_vector is not None and prev_pt is not None and predicted_center is not None:
+        if vectors_frame and mean_vector is not None and predicted_center is not None:
             # Modify the original center by the mean vector
             offset = (int(predicted_center[0] + mean_vector[0]), int(predicted_center[1] + mean_vector[1]))
-            cv2.arrowedLine(frame, predicted_center, offset, (255, 0, 255), 10)
+            if debug:
+                cv2.arrowedLine(frame, predicted_center, offset, (255, 0, 255), 10)
             predicted_center = offset
 
         # Display the matches
-        cv2.imshow("Matches", frame)
+        if debug:
+            cv2.imshow("Matches", frame)
 
-    # Update previous frame and keypoints
-    prev_gray = gray.copy()
-    prev_frame = frame.copy()
-    prev_kp = kp
-    prev_des = des
+    return predicted_center, kp, des
 
-    # Check for key press to exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-# Release video capture object and close windows
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    # Initialize feature detector
+    orb, bf = init_feature_tracking()
+
+    # Initialize template for speedometer
+    speedometer_templates = load_template_pyramid("../data/speed_template.png", 2, 1)
+
+    # Open video file
+    cap = cv2.VideoCapture("../data/audi_road_cropped.mp4")
+
+    # Initialize variables
+    template_match_history = []
+    pt_color_dict = {}
+    prev_kp, prev_des = None, None
+    predicted_center = None
+
+    # Skip to frame 3050
+    cap.set(cv2.CAP_PROP_POS_FRAMES, 3050)
+
+    while True:
+        # Read the next frame
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Find the speedometer
+        predicted_center, kp, des = find_speedometer(frame, orb, bf, prev_kp, prev_des, speedometer_templates,
+                                                     template_match_history, pt_color_dict, predicted_center,
+                                                     debug=True)
+
+        # Update previous frame and keypoints
+        prev_kp = kp
+        prev_des = des
+
+        # Check for key press to exit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release video capture object and close windows
+    cap.release()
+    cv2.destroyAllWindows()
